9e54587ca463e8975fa118fdf31f3c3495752bfd
Gingoh <dmrb.jas@gmail.com>
2023-03-14 18:57:34 -0300

    java  intro

---
+++ java/Intro/introJava/src/introJava/App.java
@@ -0,0 +1,14 @@
+package introJava;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+      String cadena = "Hola Mundo";
+      String cadena2 = cadena.toLowerCase();   
+      System.out.println(cadena);
+      
+      System.out.println(cadena2);
+      System.out.println(cadena2.toUpperCase());
+    }
+}
